6b87f9f4ad04365c19c5a4d73ac2263e4b8cab86
Corrin Barnes <corrinbarnes@gmail.com>
2015-10-06 13:32:39 -0400

    fb login works

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,14 @@
 class SessionsController < ApplicationController
   def create
     @user = User.find_or_create_from_auth_hash(auth_hash)
-    session[:user_id] = @user.id
+    sign_in @user
+    binding.pry
     redirect_to '/'
   end
 
   def destroy
-    session[:user_id] = nil
+    sign_out @user
+    redirect_to '/'
   end
 
   protected
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,12 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   def self.find_or_create_from_auth_hash(auth_hash)
-    User.find_or_create_by(provider:)
+    User.find_or_create_by(provider: auth_hash.provider, uid: auth_hash.uid) do |user|
+      user.username = auth_hash.info.name
+      user.profile_pic = auth_hash.info.image
+      user.email = auth_hash.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
   end
 
   def admin?
