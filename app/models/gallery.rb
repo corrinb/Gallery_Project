4e25294df6a67d5cce2536b1dbab6f652373465e
Corrin Barnes <corrinbarnes@gmail.com>
2015-10-08 16:04:05 -0400

    <=

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -4,6 +4,6 @@ class Gallery < ActiveRecord::Base
 
   def self.current_gallery
     now = Time.now
-    where('start_date < ? and end_date >= ?', now, now).last
+    where('start_date <= ? and end_date >= ?', now, now).last
   end
 end
--- app/models/gallery_worker.rb
+++ app/models/gallery_worker.rb
@@ -1,9 +1,13 @@
 class GalleryWorker
   def self.create_gallery
-    gallery = Gallery.new
-    gallery.theme = Theme.highest_vote.title
-    gallery.start_date = Date.today
-    gallery.end_date = Date.today.end_of_month
-    gallery.save
+    if Date.today == Date.today.beginning_of_month
+      gallery = Gallery.new
+      gallery.theme = Theme.highest_vote.title
+      gallery.start_date = Date.today
+      gallery.end_date = Date.today.end_of_month
+      gallery.save
+
+      Theme.destroy_all
+    end
   end
 end
